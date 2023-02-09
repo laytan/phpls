92c49030a3a21bcf8e1a497f4ed10894f9c09137
Laytan Laats <laytanlaats@hotmail.com>
2023-02-09 20:34:39 +0100

    fix: better multi-namespace file support

--- pkg/fqn/fqn.go
+++ pkg/fqn/fqn.go
@@ -2,9 +2,6 @@ package fqn
 
 import (
 	"strings"
-
-	"github.com/VKCOM/noverify/src/ir"
-	"github.com/laytan/elephp/pkg/symbol"
 )
 
 const PartSeperator = `\`
@@ -61,91 +58,3 @@ func (f *FQN) Parts() []string {
 
 	return parts[1:]
 }
-
-func NewTraverser() *Traverser {
-	return &Traverser{}
-}
-
-// A FQNTraverser that handles keywords like self or static.
-func NewTraverserHandlingKeywords(block ir.Node) *Traverser {
-	return &Traverser{block: block}
-}
-
-// Traverser implements ir.Visitor.
-type Traverser struct {
-	fileUses      []*ir.UseStmt
-	fileNamespace string
-
-	block      ir.Node
-	blockClass *ir.ClassStmt
-}
-
-func (f *Traverser) ResultFor(name *ir.Name) *FQN {
-	// Handle self and static by returning the class the block is in.
-	if f.block != nil && f.blockClass != nil {
-		if name.Value == "self" || name.Value == "static" {
-			name.Value = f.blockClass.ClassName.Value
-		}
-	}
-
-	if name.IsFullyQualified() {
-		return New(name.Value)
-	}
-
-	// If any use statement ends with the class name, use that.
-	for _, usage := range f.fileUses {
-		if usage.Alias != nil {
-			if usage.Alias.Value == name.LastPart() {
-				return New(PartSeperator + usage.Use.Value)
-			}
-		}
-
-		if usage.Use.LastPart() == name.LastPart() {
-			return New(PartSeperator + usage.Use.Value)
-		}
-	}
-
-	// Else use namespace+class name.
-	if f.fileNamespace != "" {
-		return New(PartSeperator + f.fileNamespace + PartSeperator + name.LastPart())
-	}
-
-	// Else use class name.
-	return New(PartSeperator + name.LastPart())
-}
-
-func (f *Traverser) EnterNode(node ir.Node) bool {
-	switch typedNode := node.(type) {
-	case *ir.ClassStmt:
-		if f.block == nil {
-			return false
-		}
-
-		bPos := ir.GetPosition(f.block)
-		nPos := typedNode.Position
-
-		// If the block is inside the class.
-		if bPos.StartLine >= nPos.StartLine && bPos.EndLine <= nPos.EndLine && bPos.StartPos >= nPos.StartPos && bPos.EndPos <= nPos.EndPos {
-			f.blockClass = typedNode
-		}
-
-		return false
-
-	case *ir.UseStmt:
-		f.fileUses = append(f.fileUses, typedNode)
-
-		return false
-
-	case *ir.NamespaceStmt:
-		if name := symbol.GetIdentifier(typedNode); name != "" {
-			f.fileNamespace = name
-		}
-
-		return false
-
-	default:
-		return !symbol.IsScope(ir.GetNodeKind(typedNode))
-	}
-}
-
-func (f *Traverser) LeaveNode(ir.Node) {}
--- pkg/fqn/fqn_test.go
+++ pkg/fqn/fqn_test.go
@@ -4,7 +4,11 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/VKCOM/noverify/src/ir"
+	"github.com/VKCOM/php-parser/pkg/position"
+	"github.com/laytan/elephp/internal/parsing"
 	"github.com/laytan/elephp/pkg/fqn"
+	"github.com/laytan/elephp/pkg/phpversion"
 	"github.com/matryer/is"
 )
 
@@ -48,6 +52,161 @@ func TestFQN(t *testing.T) {
 			is.Equal(f.String(), test.FQN)
 			is.Equal(f.Name(), test.Name)
 			is.Equal(f.Namespace(), test.Namespace)
+		})
+	}
+}
+
+func TestFQNTraverser(t *testing.T) {
+	t.Parallel()
+
+	cases := map[string]struct {
+		code   string
+		name   *ir.Name
+		expect string
+	}{
+		"single semicolon": {
+            code:   `
+            <?php
+            namespace Test;
+            // Some comment.
+            `,
+            name:   &ir.Name{Value: "TestName", Position: &position.Position{
+                StartLine: 3,
+                StartPos: 50,
+                EndLine: 3,
+                EndPos: 51,
+            }},
+			expect: "\\Test\\TestName",
+		},
+        "no namespaces": {
+            code: `
+            <?php
+            `,
+            name: &ir.Name{Value: "TestName", Position: &position.Position{
+                StartLine: 1,
+                StartPos: 1,
+                EndLine: 1,
+                EndPos: 2,
+            }},
+            expect: "\\TestName",
+        },
+        "single global namespace block": {
+            code: `
+            <?php
+            namespace {
+                // Some comment.
+            }
+            `,
+            name: &ir.Name{Value: "TestName", Position: &position.Position{
+                StartLine: 3,
+                StartPos: 20,
+                EndLine: 3,
+                EndPos: 24,
+            }},
+            expect: "\\TestName",
+        },
+        "single namespace block": {
+            code: `
+            <?php
+            namespace Test {
+                // Some comment.
+            }
+            `,
+            name: &ir.Name{Value: "TestName", Position: &position.Position{
+                StartLine: 3,
+                StartPos: 55,
+                EndLine: 3,
+                EndPos: 56,
+            }},
+            expect: "\\Test\\TestName",
+        },
+        "double semicolon": {
+            code: `
+            <?php
+            namespace Test;
+            // Some comment.
+            namespace Test2;
+            // Some comment 2.
+            `,
+            name: &ir.Name{Value: "TestName", Position: &position.Position{
+                StartLine: 3,
+                StartPos: 55,
+                EndLine: 3,
+                EndPos: 56,
+            }},
+            expect: "\\Test\\TestName",
+        },
+        "double block": {
+            code: `
+            <?php
+            namespace Test {
+                // Some comment.
+            }
+
+            namespace Test2 {
+                // Some comment 2.
+            }
+            `,
+            name: &ir.Name{Value: "TestName", Position: &position.Position{
+                StartLine: 7,
+                StartPos: 140,
+                EndLine: 7,
+                EndPos: 141,
+            }},
+            expect: "\\Test2\\TestName",
+        },
+        "mixed": {
+            code: `
+            <?php
+            namespace {
+                // Some comment.
+            }
+            namespace Test;
+            use Test\Aliassed as Alias;
+            // some comment 2.
+            `,
+            name: &ir.Name{Value: "Alias", Position: &position.Position{
+                StartLine: 6,
+                StartPos: 130,
+                EndLine: 6,
+                EndPos: 131,
+            }},
+            expect: "\\Test\\Aliassed",
+        },
+        "block with use statement": {
+            code: `
+            <?php
+            namespace Test1 {
+                use Testing\Test;
+                // Some comment.
+            }
+            `,
+            name: &ir.Name{Value: "Test", Position: &position.Position{
+                StartLine: 4,
+                StartPos: 60,
+                EndLine: 4,
+                EndPos: 61,
+            }},
+            expect: "\\Testing\\Test",
+        },
+	}
+
+	parser := parsing.New(phpversion.EightOne())
+
+	for name, test := range cases {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			is := is.New(t)
+
+			root, err := parser.Parse(test.code)
+			is.NoErr(err)
+
+			fqnt := fqn.NewTraverser()
+			root.Walk(fqnt)
+
+			result := fqnt.ResultFor(test.name)
+			is.Equal(result.String(), test.expect)
 		})
 	}
 }
